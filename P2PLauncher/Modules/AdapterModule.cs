bd87f04386c9d2d0f32f575e41e8f7bf4e8dc4f9
Kacper <kacperserewis@outlook.com>
2020-11-20 16:04:49 +0100

    Add AdapterModule to list all adapters

--- P2PLauncher/Modules/AdapterModule.cs
+++ P2PLauncher/Modules/AdapterModule.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using P2PLauncher.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Management;
+using System.Net.NetworkInformation;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -9,5 +12,27 @@ namespace P2PLauncher.Modules
     class AdapterModule
     {
 
+        /// <summary>
+        /// Detects all network adapters installed in the system
+        /// (software and hardware)
+        /// </summary>
+        /// <returns>List of network adapters</returns>
+        public List<NetworkAdapter> ListAdapters()
+        {
+            List<NetworkAdapter> networkAdapters = new List<NetworkAdapter>();
+            var query = new ObjectQuery("SELECT * FROM Win32_NetworkAdapter");
+            using(var searcher = new ManagementObjectSearcher(query))
+            {
+                var queryCollection = searcher.Get();
+                foreach(var m in queryCollection)
+                {
+                    NetworkAdapter adapter = new NetworkAdapter().FromWMI(m);
+                    networkAdapters.Add(adapter);
+                }
+            }
+            return networkAdapters;
+
+        }
+                
     }
-}
+}                                                   
