b32733ec6252540e2781571fb81a3c1345017909
Kacper <kacperserewis@outlook.com>
2020-11-23 16:55:32 +0100

    Add get ip function

---
+++ P2PLauncher/Exceptions/EnvironmentErrorException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P2PLauncher.Exceptions
+{
+    class EnvironmentErrorException : Exception
+    {
+        public EnvironmentErrorException(string message) : base(message)
+        { }
+    }
+}
---
+++ P2PLauncher/Utils/EnvironmentUtils.cs
@@ -0,0 +1,31 @@
+﻿using P2PLauncher.Exceptions;
+using P2PLauncher.Modules;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace P2PLauncher.Utils
+{
+    class EnvironmentUtils
+    {
+
+        /// <summary>
+        /// Uses HTTP request to detect client's public ip address.
+        /// </summary>
+        /// <exception cref="EnvironmentErrorException">Website returning client's ip address returned something else.</exception>
+        /// <returns>Client's public IP address.</returns>
+        public static string GetIPAddress()
+        {
+            string serverResponse = WebRequestModule.MakeGetRequest("http://checkip.dyndns.org/");
+            MatchCollection matchCollection = new Regex(@"\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}").Matches(serverResponse);
+            if(matchCollection.Count > 0)
+            {
+                throw new EnvironmentErrorException("IP request returned invalid data.");
+            }
+            return matchCollection[0].ToString();
+        }
+    }
+}
