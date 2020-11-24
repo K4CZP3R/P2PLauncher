5c63b583903ee4c6a23a2c7afb4b4a5eadebdad6
Kacper <kacperserewis@outlook.com>
2020-11-24 22:35:30 +0100

    Add configuration module.

---
+++ P2PLauncher/Models/ConfigurationModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P2PLauncher.Models
+{
+    class ConfigurationModel
+    {
+        /// <summary>
+        /// Location of the FreeLan.exe
+        /// </summary>
+        public string FreeLanExecutablePath { get; set; }
+    }
+}
